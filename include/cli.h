a8c48de8408b45807a7ce6717251d422b3307101
Enes Emre Aydogan <aydogan.ee@proton.me>
2025-08-24 16:00:16 +0300

    feat(cli): Implement CLI skeleton. Closes #4
    Add help and version functions
    Add function prototypes for other functions
    Fix build

--- include/cli.h
+++ include/cli.h
@@ -1,8 +1,8 @@
 #pragma once
 #include <stddef.h>
-typedef enum
-{
-  CMD_HELP = 0,
+const char* VER = "v0.1";
+typedef enum {
+  CMD_HELP = 1,
   CMD_VERSION,
   CMD_INIT,
   CMD_ADD,
@@ -12,23 +12,32 @@ typedef enum
   CMD_UNKNOWN,
 } cmd_type_t;
 
-typedef enum
-{
-  SCP_DOT = 0,
+typedef enum {
+  SCP_DOT = 1,
   SCP_PKG,
   SCP_DEP,
   SCP_UNKNOWN,
 } scp_type_t;
 
-typedef struct
-{
+typedef struct {
   cmd_type_t cmd;
   scp_type_t scp;
   char**     args;
   size_t     argc;
-} cli_arg_t;
+} cli_command_t;
 
-int        parse_args(int argc, char** argv, cli_arg_t* result);
+int        parse_args(int argc, char** argv, cli_command_t* result);
 cmd_type_t extract_cmd(char* cmd);
 scp_type_t extract_scp(char* scp);
-int        extract_args(char* args, cli_arg_t* result);
+int        extract_args(char* args, cli_command_t* result);
+
+int cmd_help(cli_command_t cmd);
+int cmd_version(cli_command_t cmd);
+int cmd_init(cli_command_t cmd);
+int cmd_add(cli_command_t cmd);
+int cmd_delete(cli_command_t cmd);
+int cmd_list(cli_command_t cmd);
+int cmd_backup(cli_command_t cmd);
+int cmd_unkonwn(cli_command_t cmd);
+
+int execute_cmd(cli_command_t cmd);
