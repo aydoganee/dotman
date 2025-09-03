31ffb7a4f55359a3ad8d7c6d0b7806a3f329913b
Enes Emre Aydogan <enes.e.aydogan@gmail.com>
2025-09-03 13:45:11 +0300

    refactor(config): pointer members in ConfigLine

--- include/config.h
+++ include/config.h
@@ -6,18 +6,19 @@
 
 #include "cli.h"
 #include "strarr.h"
+#include "structarr.h"
 
 #define INIT_FIELD_CAP 64
 #define INIT_ARR_CAP 32
 
 typedef struct ConfigLine {
     ScpType scp;
-    StrArr  fields;
-    StrArr  array;
+    StrArr *fields;
+    StrArr *array;
 } ConfigLine;
 
 bool ensure_capacity(char **buf, size_t *cap, size_t needed);
 bool parse_line(ConfigLine *line, FILE *file_p);
-bool parse_config(ConfigLine *result, const char *file);
+bool parse_config(StructArr *lines, const char *file);
 
 #endif
